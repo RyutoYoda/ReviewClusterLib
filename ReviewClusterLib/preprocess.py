399360c44a63e0f5e801284fe79f9a6394f3e276
RyutoYoda <98861847+RyutoYoda@users.noreply.github.com>
2024-08-13 17:20:13 +0900

    Create preprocess.py

---
+++ ReviewClusterLib/preprocess.py
@@ -0,0 +1,8 @@
+import re
+
+def preprocess_text(text):
+    text = text.lower()  # Convert to lowercase
+    text = re.sub(r'\d+', '', text)  # Remove numbers
+    text = re.sub(r'\s+', ' ', text)  # Remove extra spaces
+    text = re.sub(r'[^\w\s]', '', text)  # Remove special characters
+    return text
